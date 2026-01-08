7aa39e9d0c2a0369e444a35cb9dc1c0681af7c52
Quinten9789697 <quintkali1@gmail.com>
2026-01-08 17:36:17 +0100

    final

--- Scripts/Countdown.js
+++ Scripts/Countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("jan 1, 2026 11:36:00").getTime();
+var countDownDate = new Date("jan 18, 2026 11:36:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -20,7 +20,6 @@ var x = setInterval(function() {
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, show time elapsed
   if (distance < 0) {
     var elapsedTime = Math.abs(distance);
     var elapsedDays = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
